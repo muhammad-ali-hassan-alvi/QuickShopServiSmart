58a826eb3e55d36566b2d6ff43d816c99334f603
muhammad-ali-hassan-alvi <muhammadalialvi646@gmail.com>
2025-03-30 00:39:57 +0500

    Changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
     images: {
       domains: ["res.cloudinary.com", "raw.githubusercontent.com"],
     },
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false, path: false };
+        return config;
+      },
   };
   
   export default nextConfig;
